69d630a2204534216fd23efbe24731e80001ffd9
Pedro Muniz <phgmuniz@gmail.com>
2023-04-27 15:17:33 -0300

    Initial Commit

---
+++ SwiftUI-WeatherExercise/SwiftUI_WeatherExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherExerciseApp.swift
+//  SwiftUI-WeatherExercise
+//
+//  Created by Pedro Muniz on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
