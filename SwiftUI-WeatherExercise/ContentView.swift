41f354e0425a46a708641dee34f8714050c96b0a
Pedro Muniz <phgmuniz@gmail.com>
2023-05-15 17:57:56 -0300

    Started refactoring the View funcs

--- SwiftUI-WeatherExercise/ContentView.swift
+++ SwiftUI-WeatherExercise/ContentView.swift
@@ -11,20 +11,18 @@ struct ContentView: View {
     var body: some View {
         ZStack {
 
-            LinearGradient(colors: [.blue, Color("lightBlue")],
-                           startPoint: .topLeading,
-                           endPoint: .bottomLeading)
-                .edgesIgnoringSafeArea(.all)
+            // Uma vez que já temos a UI organizada como ela deveria ser, podemos criar structs para cada View
+            // separadamente, de forma que elas possam ser reusadas e atualizadas depois com novos dados, que variam.
+            // Isso pode ser feito manualmente ou ao clicar na View pressionando command e selecionar Extract View.
+            // Uma vez feito isso, podemos chamar a View aqui como uma func, que receberá os dados variáveis
+
+            BackgroundView(topColor: .blue, bottomColor: Color("lightBlue"))
 
             VStack {
 
-                Text("Fortaleza, CE")
-                    .font(.system(size: 32, weight: .medium, design: .default)) // design here is a default paremeter, so it could be deleted
-                    .foregroundColor(.white)
-                    .padding()
+                CityNameView(cityName: "Fortaleza, CE")
 
                 VStack(spacing: 8) {
-
                     Image(systemName: "cloud.sun.fill")
                         .renderingMode(.original)
                         .resizable()
@@ -38,7 +36,6 @@ struct ContentView: View {
                 }
 
                 HStack(spacing: 5) {
-
                     DayWheatherView(dayOfTheWeek: "SEG",
                                     iconName: "cloud.sun.fill",
                                     temperature: 31)
@@ -58,8 +55,6 @@ struct ContentView: View {
                     DayWheatherView(dayOfTheWeek: "SEX",
                                     iconName: "cloud.bolt.fill",
                                     temperature: 28)
-
-
                 }
                 .frame(width: 385, height: 100)
 
@@ -68,12 +63,9 @@ struct ContentView: View {
                 Button {
                     // Aqui vai a ação do botão, o que ele faz
                 } label: { // label especifica a aparência do botão
-                    Text("Mudar Dia da Semana")
-                        .frame(width: 280, height: 50)
-                        .background(Color.white)
-                        .font(.system(size: 20, weight: .bold))
-                        .foregroundColor(.blue)
-                        .cornerRadius(10)
+                    WeatherButton(weatherButtonTitle: "Mudar Dia da Semana",
+                                  textColor: .blue,
+                                  buttonBackgroundColor: .white)
                 }
 
                 Spacer()
@@ -88,15 +80,20 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+// Abaixo estão todas as structs como Views reutilizáveis que criamos.
+// Dica: se uma View for usada em muitos momentos no app, é melhor separá-la em um arquivo próprio
+// Se ela for específica daquele arquivo, podemos deixá-la no arquivo,
+// refatorando apenas para melhor legibilidade.
 struct DayWheatherView: View {
 
+    // Quando criamos Views como structs que serão chamadas na nossa UI,
+    // Suas variáveis e propriedades devem vir antes do seu body:
     var dayOfTheWeek: String
     var iconName: String
     var temperature: Int
 
     var body: some View {
         VStack {
-
             Text(dayOfTheWeek)
                 .font(.system(size: 16, weight: .medium))
                 .foregroundColor(.white)
@@ -112,5 +109,62 @@ struct DayWheatherView: View {
                 .font(.system(size: 28,weight: .medium))
         }
         .frame(width: 70, height: 100)
+    }
+}
+
+struct BackgroundView: View {
+
+    var topColor: Color
+    var bottomColor: Color
+
+    var body: some View {
+        LinearGradient(colors: [topColor, bottomColor],
+                       startPoint: .topLeading,
+                       endPoint: .bottomLeading)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CityNameView: View {
+
+    var cityName: String
+
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium, design: .default)) // Aqui, design é um parâmetro padrão, então ele poderia ser deletado daqui
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+struct BigCurrentWeatherView: View {
+
+    var body: some View {
+        Image(systemName: "cloud.sun.fill")
+            .renderingMode(.original)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 180, height: 180)
+            .padding()
+
+        Text("31° C")
+            .font(.system(size: 70, weight: .medium))
+            .foregroundColor(.white)
+    }
+}
+
+struct WeatherButton: View {
+
+    var weatherButtonTitle: String
+    var textColor: Color
+    var buttonBackgroundColor: Color
+
+    var body: some View {
+        Text(weatherButtonTitle)
+            .frame(width: 280, height: 50)
+            .background(buttonBackgroundColor)
+            .font(.system(size: 20, weight: .bold))
+            .foregroundColor(textColor)
+            .cornerRadius(10)
     }
 }
